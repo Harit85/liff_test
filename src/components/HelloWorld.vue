e4847414d3704da974bc60e622fe015badecdb8f
Harit85 <pharit85@gmail.com>
2023-09-26 17:25:37 +0700

    commit test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,122 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h1 class="title">VueJs Line LIFF Showcase</h1>
+    <div class="columns is-mobile">
+      <div class="column is-12 has-text-centered buttons">
+        <button @click="openWindow()" class="button is-primary">Open Window</button>
+        <button @click="closeWindow()" class="button is-info">Close Window</button>
+        <button @click="sendMessage()" class="button is-success">Send Message</button>
+        <button @click="getProfile()" class="button is-danger">Get Profile</button>
+      </div>
+    </div>
+    <h2 class="subtitle">Profile</h2>
+      <div class="columns is-mobile">
+        <div class="column is-half is-offset-one-quarter">
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">User ID:</label>
+            </div>
+            <div class="field-body">
+              <div class="field is-narrow">
+                <div class="control">
+                  {{ profile.userId || '-' }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Display Name:</label>
+            </div>
+            <div class="field-body">
+              <div class="field is-narrow">
+                <div class="control">
+                  {{ profile.displayName || '-' }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Picture URL:</label>
+            </div>
+            <div class="field-body">
+              <div class="field is-narrow">
+                <div class="control">
+                  {{ profile.pictureUrl || '-' }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label">Status Message:</label>
+            </div>
+            <div class="field-body">
+              <div class="field is-narrow">
+                <div class="control">
+                  {{ profile.statusMessage || '-' }}
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    <br>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+  },
+  data: () => ({
+    profile: {
+      userId: '',
+      displayName: '',
+      pictureUrl: '',
+      statusMessage: ''
+    }
+  }),
+  methods: {
+    openWindow () {
+      this.$liff.openWindow({
+        url: 'https://developers.line.me/en/docs/liff/overview/'
+      })
+    },
+    closeWindow () {
+      this.$liff.closeWindow()
+    },
+    sendMessage () {
+      this.$liff.sendMessages([
+        {
+          type: 'text',
+          text: 'You/\'ve successfully sent a message! Hooray!'
+        },
+        {
+          type: 'sticker',
+          packageId: '2',
+          stickerId: '144'
+        }
+      ]).then(function () {
+        window.alert('Message sent')
+      }).catch(function (error) {
+        window.alert('Error sending message: ' + error)
+      })
+    },
+    getProfile () {
+      let _this = this
+      this.$liff.getProfile().then(function (profile) {
+        _this.profile = profile
+      }).catch(function (error) {
+        alert('Error getting profile: ' + error)
+      })
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import liff from '@line/liff';
 
 createApp(App).mount('#app')
+liff.init({
+    liffId: '2000904850-8nYxV5dd', // Use own liffId
+});
+console.log(liff.getLanguage());
+console.log(liff.getVersion());
+console.log(liff.isInClient());
+console.log(liff.isLoggedIn());
+console.log(liff.getOS());
+console.log(liff.getLineVersion());
